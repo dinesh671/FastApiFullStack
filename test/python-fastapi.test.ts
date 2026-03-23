594496d0f9ccfb884395b7e8e1e904e03130c84c
DvsDineshKumar <dinesh671@yahoo.com>
2026-03-23 12:45:14 +0530

    Initial commit

---
+++ test/python-fastapi.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PythonFastapi from '../lib/python-fastapi-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/python-fastapi-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PythonFastapi.PythonFastapiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
